40dbbd581cec2ad2abb6a1b9ae1d5d116867b3be
Harishe328 <harishe328@gmail.com>
2024-01-29 19:05:18 +0530

    updated

---
+++ conditionstatements.js
@@ -0,0 +1,42 @@
+let a=1
+//if
+if(a<10){
+    console.log("success")
+}
+
+//if else
+if(a>10){
+    console.log("success")
+}else{
+    console.log("success")
+}
+
+//else if
+if(a==1){
+    console.log("one")
+}
+else if(a<10){
+    console.log("success");
+}else{
+    console.log("fail");
+}
+
+//switch
+let b=2;
+switch(b){
+    case 1:{
+        console.log("One");
+        break;
+    }
+    case 2:{
+        console.log("Two");
+        break;
+    }
+    case 3:{
+        console.log("Three");
+        break;
+    }
+    default:{
+        console.log("condition not matched")
+    }
+}
